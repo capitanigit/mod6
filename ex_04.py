f26906c3ba8edce8cbe6afbcffc66e6bbb4c62e0
Krzysiek Krasowski <Krzysiek.Krasowski@gmail.com>
2022-10-24 12:16:11 +0100

    Update

--- ex_04.py
+++ ex_04.py
@@ -48,10 +48,35 @@ def select_where(conn, table, **query):
    print("",rows)
    return rows
 
+def update(conn, table, id, **kwargs):
+   """
+   update status, begin_date, and end date of a task
+   :param conn:
+   :param table: table name
+   :param id: row id
+   :return:
+   """
+   parameters = [f"{k} = ?" for k in kwargs]
+   parameters = ", ".join(parameters)
+   values = tuple(v for v in kwargs.values())
+   values += (id, )
+
+   sql = f''' UPDATE {table}
+             SET {parameters}
+             WHERE id = ?'''
+   try:
+       cur = conn.cursor()
+       cur.execute(sql, values)
+       conn.commit()
+       print("OK")
+   except sqlite3.OperationalError as e:
+       print(e)
+
 if __name__ == "__main__":
    conn = create_connection("database.db")
-   select_all(conn, "projects")
-   select_all(conn, "tasks")
-   select_where(conn, "tasks", projekt_id=2)
-   select_where(conn, "tasks", status="Ended")
+   #select_all(conn, "projects")
+   #select_all(conn, "tasks")
+   #select_where(conn, "tasks", projekt_id=2)
+   #select_where(conn, "tasks", status="Ended")
+   update(conn, "tasks", 2, status="Ended")
    conn.close()
