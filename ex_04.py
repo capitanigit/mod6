1e3f341a4e006e484dee7f05896cee98fbe3fd19
Krzysiek Krasowski <Krzysiek.Krasowski@gmail.com>
2022-10-24 12:20:24 +0100

    Delete

--- ex_04.py
+++ ex_04.py
@@ -71,6 +71,39 @@ def update(conn, table, id, **kwargs):
        print("OK")
    except sqlite3.OperationalError as e:
        print(e)
+def delete_where(conn, table, **kwargs):
+   """
+   Delete from table where attributes from
+   :param conn:  Connection to the SQLite database
+   :param table: table name
+   :param kwargs: dict of attributes and values
+   :return:
+   """
+   qs = []
+   values = tuple()
+   for k, v in kwargs.items():
+       qs.append(f"{k}=?")
+       values += (v,)
+   q = " AND ".join(qs)
+
+   sql = f'DELETE FROM {table} WHERE {q}'
+   cur = conn.cursor()
+   cur.execute(sql, values)
+   conn.commit()
+   print("Deleted")
+
+def delete_all(conn, table):
+   """
+   Delete all rows from table
+   :param conn: Connection to the SQLite database
+   :param table: table name
+   :return:
+   """
+   sql = f'DELETE FROM {table}'
+   cur = conn.cursor()
+   cur.execute(sql)
+   conn.commit()
+   print("Deleted")
 
 if __name__ == "__main__":
    conn = create_connection("database.db")
@@ -78,5 +111,7 @@ if __name__ == "__main__":
    #select_all(conn, "tasks")
    #select_where(conn, "tasks", projekt_id=2)
    #select_where(conn, "tasks", status="Ended")
-   update(conn, "tasks", 2, status="Ended")
+   #update(conn, "tasks", 2, status="Ended")
+   delete_where(conn, "tasks", id=3)
+   #delete_all(conn, "tasks")
    conn.close()
